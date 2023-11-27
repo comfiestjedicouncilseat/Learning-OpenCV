199206309d1c6f63ea4603e41e2ffb99a8b4f11d
Michael Tao <michaeltao0713@gmail.com>
2023-11-27 10:44:06 -0500

    changed how noice affects the face detection

--- face_detect_vid.py
+++ face_detect_vid.py
@@ -8,11 +8,14 @@ haar_cascade = cv.CascadeClassifier("haar_face.xml")
 
 while True:
     isTrue, frame = capture.read()
+    if not isTrue:
+        break
+
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     # detect the face
     faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1,
-                                               minNeighbors=1)
+                                               minNeighbors=7)
 
     # drawing a rectangle over faces
     for (x, y, w, h) in faces_rect:
